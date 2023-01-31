e08203f831be3225d9abb24e1e7f1558e080c830
grdddj <jiri.musil06@seznam.cz>
2023-01-31 18:13:40 +0100

    add mock config

---
+++ config.py
@@ -0,0 +1,17 @@
+class Config:
+    """To be filled and renamed to config.py."""
+
+    subject = "foo"
+
+    port = 465
+    smtp_server = "smtp.gmail.com"
+    sender_email = "foo@gmail.com"
+    password = "foo"
+
+    recipients = [
+        "foo@foo.cz",
+    ]
+
+    example_text = "foo!"
+
+    PUSHBULLET_TOKEN = "o.foo"
